052faf5c8da36aa6b9fa8eca90d69d0fc578b399
Gustavo <gustavogsousa@gmail.com>
2021-07-06 16:34:18 -0300

    crudo em rails

--- app/helpers/veiculos_helper.rb
+++ app/helpers/veiculos_helper.rb
@@ -1,2 +1,4 @@
 module VeiculosHelper
+
+
 end
--- app/models/veiculo.rb
+++ app/models/veiculo.rb
@@ -1,5 +1,6 @@
 class Veiculo < ApplicationRecord
-
+  validates_presence_of :placa, :message => " - Deve ser preenchido"
+  validates_presence_of :renavam, :message => " - Deve ser preenchido"
   belongs_to :proprietario
 
 end
